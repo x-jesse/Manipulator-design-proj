f90db9e1eafd6035939aa3b31bc5431baee4debb
Jesse Xia <68565085+x-jesse@users.noreply.github.com>
2021-04-12 20:05:13 -0400

    Add pseudocode

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ import math
 
 # TORQUE CALCULATIONS
 
-# CASE X = 0.75M, Y = 0.1M, THETA = -60
-hyp = math.sqrt(0.75**2 + 0.1**2)
+# CASE X = 0.75M, Y = 0.1M, THETA = -60 WRT X
 
+# CASE X = 0.5M, Y = 0.5M, THETA = 0 WRT X
+
+# CASE X = 0.2M, Y = 0.6M, THETA = 45 WRT X
 
 
 
