7d521bbf6c5b135458b79328898e1ecb86e49d19
Jesse Xia <68565085+x-jesse@users.noreply.github.com>
2021-04-12 20:00:39 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import math
+
+###################################
+# MANIPULATOR DESIGN PROJ MTE 119 #
+###################################
+
+# TORQUE CALCULATIONS
+
+# CASE X = 0.75M, Y = 0.1M, THETA = -60
+hyp = math.sqrt(0.75**2 + 0.1**2)
+
+
+
+
+
