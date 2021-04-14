900cb3d7ff8d2955fbaa7432d7622f5fcf5c221b
Jesse Xia <68565085+x-jesse@users.noreply.github.com>
2021-04-13 20:18:01 -0400

    code working?

--- main.py
+++ main.py
@@ -5,11 +5,6 @@ import math
 ######################################
 
 
-def distance(x, y):
-    d = math.sqrt(0.75**2 + 0.1**2)
-    return d
-
-
 def torque(l1, l2, l3, x, y, q3):
     delta_x, delta_y, = x - l3*math.cos(q3), y - l3*math.sin(q3)
 
@@ -18,10 +13,10 @@ def torque(l1, l2, l3, x, y, q3):
         q1 = math.atan(delta_y/delta_x) - math.atan(l2*math.sin(q2)/(l1-l2*math.cos(q2)))
     except ValueError:
         # print("breaks", l1, l2, l3)
-        return 999
+        return 999, 0, 0
     else:
-        t = 0.5*l1*4*l1*math.sin(q1) + 0.5*l2*2*l2*math.sin(q2) + 0.5*l3*l3*5*9.81*math.sin(q3)
-        return t
+        t = 0.5*9.81*(l1*4*l1*math.sin(q1) + l2*2*l2*math.sin(q2) + l3*l3*5*math.sin(q3))
+        return t, q1, q2
 
 # TORQUE CALCULATIONS
 
@@ -36,12 +31,12 @@ x2, y2 = 0.5, 0.5
 # CASE X = 0.2M, Y = 0.6M, THETA = 45 WRT X
 x3, y3 = 0.2, 0.6
 
-cases = [(x1, y1, -60), (x2, y2, 0), (x3, y3, 45)]
+cases = [(x1, y1, math.radians(-60)), (x2, y2, 0), (x3, y3, math.radians(45))]
 
-mint = 100
+min_torque = 100
 a, b, c = 0, 0, 0
+q1, q2, q3 = 0, 0, 0
 
-q3 = -60
 for l in range(50):
     l3 = l/100
     for ll in range(50):
@@ -52,13 +47,15 @@ for l in range(50):
                 continue
 
             # print(l1, l2, l3)
-            temp = 999
-            for c in cases:
-                x, y, q3 = c
-                temp += torque(l1, l2, l3, x, y, q3)**2
+            torque_val = 999
+            for case in cases:
+                x, y, q3 = case
+                temp_torque, a1, a2 = torque(l1, l2, l3, x, y, q3)
+                torque_val += temp_torque**2
 
-            if abs(temp)**0.5 < mint:
-                mint = temp**0.5
+            if abs(torque_val)**0.5 < min_torque:
+                min_torque = torque_val ** 0.5
                 a, b, c = l1, l2, l3
+                q1, q2 = a1, a2
 
-    print(mint, a, b, c)
+    print(min_torque, "l1 =", a, "q1 =", q1, "l2 =", b, "q2 =", q2, "l3 =", c)
